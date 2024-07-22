026d352975101ff4629c3c835cdccfe38d945279
Isaac-Best <bestis@oregonstate.edu>
2024-07-21 18:42:21 -0700

    test

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -28,9 +28,7 @@ async function handler(req, res){
         const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.1zrcmhr.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
 
         try{
-            client = await MongoClient.connect(
-                'mongodb+srv://Isaac:TfMAwaf4gn6lzjbd@cluster0.1zrcmhr.mongodb.net/my-site?retryWrites=true&w=majority'
-                );
+            client = await MongoClient.connect(connectionString);
         } catch (error){
             res.status(500).json({message: 'Could not connect to database.'})
             return;
